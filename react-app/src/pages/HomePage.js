0f4fffdeecec919e8df0eb818fb70eea0c24a895
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-07-19 09:25:09 -0400

    Logo?

--- react-app/src/pages/HomePage.js
+++ react-app/src/pages/HomePage.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.svg';
-import cs3214 from '../images/cs3214.png';
+import logo_name from '../images/logo_name.png';
 
 let HomePage = (props) => (
   <Container>
     <Row>
-      <h1>CS3214 Demo App</h1>
+      <h1>Website Demo App</h1>
     </Row>
     <Row>
       <Col>
         <img alt="" src={logo} className="app-logo" />
       </Col>
       <Col>
-        <img alt="" src={cs3214} className="cs3214-logo" />
+        <img alt="" src={logo_name} className="name-logo" />
       </Col>
     </Row>
     <Row>
