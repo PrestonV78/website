54e07153336186eaf192e600b1887aab58218ec1
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-08-25 11:15:22 -0400

    Fix Pages

---
+++ react-app/src/pages/PublicPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const PublicPage = () => (
+  <Container>
+    <h1>Welcome to a public page</h1>
+    <div>
+      This Public page is meaningless.
+      </div>
+  </Container>);
+
+export default PublicPage;
