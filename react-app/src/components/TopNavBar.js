6812eb1c51ae52b623cb72698a07c6c962f5b685
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-09-06 11:48:02 -0400

    Nav Logo v7

--- react-app/src/components/TopNavBar.js
+++ react-app/src/components/TopNavBar.js
@@ -68,7 +68,7 @@ const NavBar = (props) => {
         <NavbarToggler onClick={toggle} />
         <NavbarBrand to="/">
           {/* {props.branding} */}
-          <img width="70px" height="auto" className="img-responsive" src="/logo.svg"  alt="logo" />{' '}
+          <img width="70px" height="auto" className="img-responsive" src="/logo.svg"  alt="" />{' '}
           {/* <img
             src="logo.svg"
             width="30"
