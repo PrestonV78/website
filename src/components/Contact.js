56e59e4858f1229308f59b9d4bae53f2affa68d5
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-10-15 10:41:01 -0400

    Template Working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,3 @@
-// src/components/Contact.js
-
 import React from "react";
 
 export default function Contact() {
@@ -20,7 +18,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+      body: encode({ "form-name": "test", name, email, message }),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
@@ -67,7 +65,7 @@ export default function Contact() {
         </div>
         <form
           netlify
-          name="contact"
+          name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
