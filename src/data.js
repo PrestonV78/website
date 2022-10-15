b9f9a0e70e5f99ab85278adbed6af4631841706f
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-10-15 12:24:08 -0400

    Experiences Added

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Coming Soon",
+    subtitle: "Coming Soon",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Coming Soon",
+    subtitle: "Coming Soon",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Coming Soon",
+    subtitle: "Coming Soon",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Coming Soon",
+    subtitle: "Coming Soon",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "",
   },
 ];
 
@@ -63,4 +63,23 @@ export const courses = [
   "Multivariable Calculus",
   "Software Design",
   "Single Variable Calculus",
+];
+
+export const experiences = [
+  {
+    title: "Virginia Tech",
+    subtitle: "Education",
+    description:
+      "Attended Virginia Polytechnic Instituate and State Universiry from July 2019 to May 2023. Graduating with a Bachelor's in Computer Science Engineering with a Minor in Mathematics.",
+    image: "./VirginiaTech.svg",
+    link: "https://vt.edu",
+  },
+  {
+    title: "ThermoFlyAI",
+    subtitle: "GUI Team Lead",
+    description:
+      "While attending Virginia Tech I participated in the ECE Major Design Experience class and was a part of the ThermoFlyAI team. I was the GUI/FrontEnd Team Lead on a project to develop an AI model to control a Residential HVAC System to allow for the learning of a User's preferred temperature based on a standard model and individual learning.",
+    image: "./ThermoFlyAI.svg",
+    link: "",
+  },
 ];
