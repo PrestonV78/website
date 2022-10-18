1306e45213248e05bf2434bb114ded691d63f366
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-10-18 19:57:04 -0400

    Spacing

--- src/components/Space.js
+++ src/components/Space.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Space() {
 return (
     <section id="space" className="relative">
-      <div className="container mx-auto flex px-10 py-40 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
       </div>
     </section>
   );
