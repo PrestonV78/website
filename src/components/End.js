14db299124a94c36aa8033b4176b0fc5967f6083
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-10-18 16:28:25 -0400

    Copyright v2

--- src/components/End.js
+++ src/components/End.js
@@ -5,9 +5,9 @@ return (
     <section id="end" className="relative">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <h1 className="sm:text-4xl text-3xl font-small title-font mb-4 text-white">
+          <p className="mb-8 leading-relaxed">
             Copyright Preston Verling 2023
-          </h1>
+          </p>
         </div>
       </div>
     </section>
