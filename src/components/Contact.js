3d156003cb558db7baf7b9131b7c328bcbd924f9
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-10-18 15:56:04 -0400

    Netlify v4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,7 +55,8 @@ export default function Contact() {
             <label htmlFor="affiliation" className="leading-7 text-sm text-gray-400">
               Affiliation
             </label>
-            <textarea
+            <input
+              type="text"
               id="affiliation"
               name="affiliation"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
