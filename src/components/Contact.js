29b9d7863b78e9b0a138a3f1e6ec481d908e416b
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-11-09 22:34:55 -0500

    Split Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ export default function Contact() {
           netlify
           name="test"
           onSubmit={handleSubmit}
-          className="lg:w md:w- flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-7xl text-3xl mb-1 font-HelloZone title-font text-center">
             Contact  Me
           </h2>
