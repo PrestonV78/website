b3596588171488f96ac2d470a59231e33040f932
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2023-10-06 08:30:20 -0500

    Fixed subcomponent pathing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Space from "./components/Space";
-import Experience from "./components/Experience";
+import About from "./About";
+import Contact from "./Contact";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Space from "./Space";
+import Experience from "./Experience";
 
 export default function Home() {
   return (
