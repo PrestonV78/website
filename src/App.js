148df78d222fa9217ac07c327b8b45be7c1fbf7c
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2023-10-06 08:26:43 -0500

    fixed missing Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { Routes, Route, Router } from "react-router-dom";
 
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Space from "./components/Space";
-import Experience from "./components/Experience";
 import End from "./components/End";
 
 export default function App() {
