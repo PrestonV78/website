a9b88cb453de5bb87f26fc915f1adeb40f3f42b8
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2024-07-02 13:14:19 -0500

    Revert to Single Page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import { Routes, Route, Router } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import End from "./components/End";
-import About from "./About";
-import Contact from "./Contact";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import Experience from "./Experience";
 
 export default function App() {
   return (
@@ -17,11 +12,6 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/projects" element={<Projects />} />
-          <Route exact path="/experience" element={<Experience />} />
-          <Route exact path="/skills" element={<Skills />} />
-          <Route exact path="/contact" element={<Contact />} />
         </Routes>
         <End />
       </Router>
