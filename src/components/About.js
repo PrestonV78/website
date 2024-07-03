728507bbb7df02d3f1eb2aea2d0542c471830953
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2024-07-03 08:51:18 -0500

    7/2024 Update

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,12 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-7xl text-3xl mb-4 font-HelloZone text-white">
-            Hi, my name is Preston.
+          Hello,
+          <br></br>My name is Preston.
             <br></br> I build creative solutions.
           </h1>
           <p className="mb-8 text-gray-400 leading-relaxed">
-            I am currently looking for Post-Grad Full Time Opportunities.<br></br>
+            I am currently working at Lockheed Martin Aeronautics.<br></br>
             I am a Virginia Tech alumni.<br></br>
             I am graduated with a Bachelor's in Computer Science.<br></br>
            
--- src/components/End.js
+++ src/components/End.js
@@ -6,7 +6,7 @@ return (
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <p className="mb-8 leading-relaxed">
-            Copyright 2023 Preston Verling
+            Copyright 2024 Preston Verling
           </p>
         </div>
       </div>
